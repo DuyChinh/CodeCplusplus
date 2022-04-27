f0b7c923c2a59d21dc3b114b0c7b3a27951893b3
doanchinhit21 <doanchinhit21@gmail.com>
2022-04-27 22:38:47 +0700

    add file CPP0115.cpp

---
+++ CPP0115.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define faster() ios_base :: sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+	faster();
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		long long n;
+		cin >> n;
+		map <int, int> mp;
+		int i = 2;
+		while (n > 1)
+		{
+			if (n % i == 0)
+			{
+				mp[i]++;
+				n /= i;
+			}
+			else
+			{
+				i++;
+			}
+		}
+		for (auto it : mp)
+		{
+			cout << it.first << " " << it.second << " ";
+		}
+		cout << endl;
+	}
+}
